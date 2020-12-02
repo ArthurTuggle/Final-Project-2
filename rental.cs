4896bb1b3b47cd3d009d604db4358d6a4f17759d
ArthurTuggle <arthurtuggle57@yahoo.com>
2020-12-02 05:38:02 +0000

    Initial commit

---
+++ rental.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace RentalApp
+{
+    internal class Rental
+    {
+        public Book Book;
+        public DateTime DateRented;
+        public DateTime DateDue;
+
+        
+        public Rental(Book book, DateTime dateDue)
+        {
+            Book = book;
+            DateRented = DateTime.Today;
+            DateDue = dateDue;
+        }
+    }
+}
